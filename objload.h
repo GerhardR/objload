03f7e326ed00fb199975a6678935c666640f0904
Gerhard Reitmayr <reitmayr@icg.tugraz.at>
2012-06-13 22:54:47 +0200

    FreeBSD license added, namespace, code simplification

--- objload.h
+++ objload.h
@@ -1,3 +1,26 @@
+/* Copyright (c) 2012, Gerhard Reitmayr
+   All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
 #ifndef OBJLOAD_H_
 #define OBJLOAD_H_
 
@@ -7,6 +30,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
+
+namespace obj {
 
 struct Model {
     std::vector<float> vertex; // 3 * N entries
@@ -55,8 +80,7 @@ inline std::ostream & operator<<( std::ostream & out, const ObjModel::FaceVertex
 template <typename T>
 inline std::istream & operator>>(std::istream & in, std::vector<T> & vec ){
     T temp;
-    in >> temp;
-    if(in) 
+    if(in >> temp) 
         vec.push_back(temp);
     return in;
 }
@@ -64,19 +88,17 @@ inline std::istream & operator>>(std::istream & in, std::vector<T> & vec ){
 inline std::istream & operator>>( std::istream & in, ObjModel::FaceVertex & f){
     int val;
     if(in >> f.v){
-        if(in.good()){
+        if(in.peek() == '/'){
+            in.get();
+            in >> f.t;
+            in.clear();
             if(in.peek() == '/'){
                 in.get();
-                in >> f.t;
+                in >> f.n;
                 in.clear();
-                if(in.peek() == '/'){
-                    in.get();
-                    in >> f.n;
-                    in.clear();
-                }
             }
-            in.clear();
         }
+        in.clear();
         --f.v;
         --f.t;
         --f.n;
@@ -216,5 +238,7 @@ inline std::ostream & operator<<( std::ostream & out, const Model & m ){
     }
     return out;
 }
+
+} // namespace obj
 
 #endif // OBJLOAD_H_
